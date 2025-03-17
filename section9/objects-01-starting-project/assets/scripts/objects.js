bc0cae5124e94463ee881cfd736375955fe308b0
kgamgak@gmail.com <kgamgak@gmail.com>
2025-03-17 18:50:21 +0900

    study javascript

--- section9/objects-01-starting-project/assets/scripts/objects.js
+++ section9/objects-01-starting-project/assets/scripts/objects.js
@@ -52,3 +52,53 @@ console.log(person['1.5']); // 자바스크립트에서 1.5를 문자열로 강�
 console.log(person);
 */
 
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+
+const movies = [];
+
+const renderMovies = () => {
+  const movieList = document.getElementById('movie-list');
+  
+  if (movies.length === 0) {
+    movieList.classList.remove('visible');
+    return;
+  } else {
+    movieList.classList.add('visible');
+  }
+  movieList.innerHTML = '';
+
+  movies.forEach((movie) => {
+    const movieEl = document.createElement('li');
+    movieEl.textContent = movie.info.title;
+    movieList.append(movieEl);
+  });
+};
+
+const addMovieHandler = () => {
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
+
+  if (
+    title.trim() === '' ||
+    extraName.trim() === '' ||
+    extraValue.trim() === ''
+  ) {
+    return;
+  }
+
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue
+    },
+    id: Math.random()
+  };
+
+  movies.push(newMovie);
+  // console.log(newMovie);
+  renderMovies();
+};
+
+addMovieBtn.addEventListener('click', addMovieHandler);
